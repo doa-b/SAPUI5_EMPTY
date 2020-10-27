1e2c242221c7c58f08f5692ac3655bf3a8ebd23a
Doa Bonifacio <50482180+doa-b@users.noreply.github.com>
2020-10-27 08:36:39 +0100

    Finished step 19 Reuse Dialogs

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,16 +1,14 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+], function (Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-
-		onShowHello : function () {
-			// show a native JavaScript alert
-			/* eslint-disable no-alert */
-			alert("Hello World");
-			/* eslint-enable no-alert */
+		onOpenDialog : function () {
+			this.getOwnerComponent().openHelloDialog();
 		}
 	});
-
 });
+
+// https://sapui5.hana.ondemand.com/#/topic/4cfa60872dca462cb87148ccd0d948ee
